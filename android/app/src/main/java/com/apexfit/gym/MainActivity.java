8810551b77922b241a4148e3888468b4920d5015
srn45 <soubhagyaranjannayak45@gmail.com>
2026-07-26 11:53:43 +0530

    capacitor add

---
+++ android/app/src/main/java/com/apexfit/gym/MainActivity.java
@@ -0,0 +1,5 @@
+package com.apexfit.gym;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.apexfit.gym',
+  appName: 'ApexFit',
+  webDir: 'dist/gym-management-system'
+};
+
+export default config;
